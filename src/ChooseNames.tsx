28ea07182bd1b5b4df65c67e12b2e48ca7db1d31
rpet064 <93119631+rpet064@users.noreply.github.com>
2022-04-21 21:49:09 +1200

    refactor chooseNames Component in progress - add component feature to catch team name values

--- src/ChooseNames.tsx
+++ src/ChooseNames.tsx
@@ -6,21 +6,27 @@ import SelectRobot from "./SelectRobot";
 export default function ChooseNames() {
   const [teams, chooseTeams] = useState(false);
 
+  function catchTeamNames(event: any){
+    const teamNames = event.target.value;
+     console.log(teamNames);
+
+  }
+
   return !teams ? (
     <div>
       <Title bigTitle="Who's Competing Today?" />
-      <form>
+      <form onSubmit="event.preventDefault()">
         <div>
           <h3> Team One </h3>
-          <input type="text" name="nameOne" placeholder="CatBots" />
+          <input type="text" name="nameOne" placeholder="CatBots" onSubmit={catchTeamNames}/>
         </div>
         <div>
           <h3> Team Two </h3>
-          <input type="text" name="nameTwo" placeholder="DogBots" />
+          <input type="text" name="nameTwo" placeholder="DogBots" onSubmit={catchTeamNames} />
         </div>
         <Button
           className="form-button"
-          type="button"
+          type="submit"
           onClick={() => chooseTeams(true)}
         >
           Submit
