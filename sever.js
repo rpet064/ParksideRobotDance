6814e6f4043489c6252385e94a9629625808d025
rpet064 <rpether@hotmail.co.nz>
2022-06-12 18:05:43 +1200

    deploy heroku

--- sever.js
+++ sever.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const path = require('path');
 const express = require('express');
 const http = require('http');
 
@@ -10,6 +9,10 @@ const server = http.Server(app);
 const port = process.env.PORT || 8080;
 
 app.use('/', express.static(path.join(__dirname, 'testheroku')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 server.listen(port, () => {
   console.log(`Listening on http://localhost:${port}/`);
